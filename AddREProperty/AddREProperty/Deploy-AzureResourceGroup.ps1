6037085d2641ce477d9997620c4714f5f7b34895
Bob German <bgerman1@hotmail.com>
2018-05-11 14:46:18 -0400

    Removed excess API connectors

--- AddREProperty/AddREProperty/Deploy-AzureResourceGroup.ps1
+++ AddREProperty/AddREProperty/Deploy-AzureResourceGroup.ps1
@@ -2,7 +2,7 @@
 
 Param(
     [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
-    [string] $ResourceGroupName = 'NewREProperties',
+    [string] $ResourceGroupName = 'AddREProperty',
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
